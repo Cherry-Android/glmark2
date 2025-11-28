66d941530706c9e6e84dff32475a018dd87c1aa5
khuongtrinh2 <khuong.trinh.xg@renesas.com>
2025-11-28 22:35:03 +0700

    glmark2: Update minSdkVersion, targetSdkVersion and APP_PLATFORM
    Signed-off-by: khuongtrinh2 <khuong.trinh.xg@renesas.com>

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_ABI := all
 APP_STL := c++_static
-APP_PLATFORM := android-16
+APP_PLATFORM := android-21
 APP_BUILD_SCRIPT := jni/Android.ndk.mk
 APP_CPPFLAGS := -std=c++17
 APP_CPPFLAGS += -fexceptions
